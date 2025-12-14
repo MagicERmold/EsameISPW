92c049941fb6ff9e2c87a07d3be8c7c5da3e66bf
MagicERmold <andrea.tarlev@studentsuniroma2.eu>
2025-12-14 11:45:10 +0100

    Prova di SonarCloud

--- EserciziAggiuntivi/src/es1/FileTicketDAO.java
+++ EserciziAggiuntivi/src/es1/FileTicketDAO.java
@@ -11,28 +11,8 @@ public class FileTicketDAO implements TicketDAO {
         try (
                 // Prova a creare i FILE
                 FileOutputStream out = new FileOutputStream(nomeFile);
-                ObjectOutputStream oos = new ObjectOutputStream(out);
+                ObjectOutputStream oos = new ObjectOutputStream(out)
                 ){
-            // Scrivi sui file
-            oos.writeObject(t);
-            System.out.println("Ticket salvato con sucesso!");
-
-        } catch (IOException e) {
-            // Se non vengono creati i file lancia l'eccezione
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public void pesceSpada(Ticket t) {
-        // Imposto il nome del file con la targa
-        String nomeFile = "ticket_" + t.getTarga() + ".dat";
-
-        try (
-                // Prova a creare i FILE
-                FileOutputStream out = new FileOutputStream(nomeFile);
-                ObjectOutputStream oos = new ObjectOutputStream(out);
-        ){
             // Scrivi sui file
             oos.writeObject(t);
             System.out.println("Ticket salvato con sucesso!");
